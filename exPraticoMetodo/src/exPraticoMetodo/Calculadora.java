1d3d67bc21c3dc3aa3b5feb6aeafd633eda65342
LeticiaPossasFranca <Leticia.possasf@gmail.com>
2022-05-05 12:28:17 -0300

    Exercícios práticos sobre métodos

---
+++ exPraticoMetodo/src/exPraticoMetodo/Calculadora.java
@@ -0,0 +1,21 @@
+package exPraticoMetodo;
+
+public class Calculadora {
+
+	public static void soma(double numero1, double numero2) {
+		double resultado = numero1 + numero2;
+		System.out.println(resultado);
+}
+	public static void subtracao(double numero1, double numero2) {
+		double resultado = numero1 - numero2;
+		System.out.println(resultado);
+}
+	public static void multiplicacao(double numero1, double numero2) {
+		double resultado = numero1 * numero2;
+		System.out.println(resultado);
+}
+	public static void divisao(double numero1, double numero2) {
+		double resultado = numero1 / numero2;
+		System.out.println(resultado);
+}
+}
---
+++ exPraticoMetodo/src/exPraticoMetodo/Main.java
@@ -0,0 +1,27 @@
+package exPraticoMetodo;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		// calculadora
+		System.out.println("Exercício Calculdadora");
+		Calculadora.soma(3, 6);
+		Calculadora.subtracao(8, 4);
+		Calculadora.multiplicacao(5, 8);
+		Calculadora.divisao(5, 2);
+
+		//Mensagem 
+		System.out.println("Exercício Mensagem");
+		Mensagem.obterMensagem(9);
+		Mensagem.obterMensagem(14);
+		Mensagem.obterMensagem(22);
+		
+		//Empréstimo
+		System.out.println("Exercício de Emprestimo");
+		Emprestimo.calcular(1000, Emprestimo.getDuasParcelas());
+		Emprestimo.calcular(1000, Emprestimo.getTresParcelas());
+		Emprestimo.calcular(1000, 5);
+
+	}
+}
